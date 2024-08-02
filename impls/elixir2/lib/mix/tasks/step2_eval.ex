fa9fc0ba9a49beadb53468d7ee8c569a19357060
Timbo <tim@refsix.com>
2024-08-02 13:33:05 +0100

    Added basic operators and function calls

--- impls/elixir2/lib/mix/tasks/step2_eval.ex
+++ impls/elixir2/lib/mix/tasks/step2_eval.ex
@@ -15,8 +15,26 @@ defmodule Mix.Tasks.Step2Eval do
     Mal.Reader.read_str(input)
   end
 
-  defp eval(input) do
-    input
+  defp eval_ast({:symbol, ast}, env) do
+    case env[ast] do
+      nil -> {:error, "Symbol not found: #{ast}"}
+      val -> val
+    end
+  end
+
+
+
+  defp eval_ast({:list, [fnSym | rest]}, env) do
+    # This is a special case for now
+    thefn = eval_ast(fnSym, env)
+    args = rest |> Enum.map(fn(x) -> eval_ast(x, env) end)
+    apply(thefn, args)
+  end
+  defp eval_ast([] = l, _), do: l
+  defp eval_ast(base, _) when is_number(base), do: base
+
+  defp eval(input, env) do
+    eval_ast(input, env)
   end
 
   defp print(input) do
@@ -25,8 +43,14 @@ defmodule Mix.Tasks.Step2Eval do
 
   defp read_eval_print(:eof), do: exit(:normal)
   defp read_eval_print(line) do
+    env = %{
+      "+" => fn (a, b) -> a + b end,
+      "-" => fn (a, b) -> a - b end,
+      "*" => fn (a, b) -> a * b end,
+      "/" => fn (a, b) -> a / b end,
+    }
     read(line)
-      |> eval
+      |> (&eval(&1, env)).()
       |> print
   catch
     {:error, msg} -> "Error: #{msg}"
